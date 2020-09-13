d371ae9063bea54a73687d52d627bfaa03aa940e
Indrajit Kumar <indrajit.kumar@philips.com>
2020-09-13 11:17:36 +0530

    commented database which i applied for Heroku

--- stories_project/settings.py
+++ stories_project/settings.py
@@ -81,12 +81,12 @@ DATABASES = {
         'NAME': os.path.join('BASE_DIR', 'db.sqlite3'),
     }
 }
-
-# ie if Heroku server
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-
-    DATABASES = {'default': dj_database_url.config()}
+#
+# # ie if Heroku server
+# if 'DATABASE_URL' in os.environ:
+#     import dj_database_url
+#
+#     DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
